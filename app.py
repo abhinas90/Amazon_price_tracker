a1f79b2db9d4c06cc561d18d3b33304547770bef
Abhinas <abhinas90@gmail.com>
2020-10-15 23:38:52 -0700

    get price if price is not in first base page

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ class Bot(object):
     def __init__(self, asin):
         self.amazon_url = "https://www.amazon.com/dp/"
         self.asin = asin
-        headers = {"authority": "www.amazon.com",
-                   "method": "GET",
-                   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
+        self.headers = {"authority": "www.amazon.com",
+                        "method": "GET",
+                        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
 
-        r = requests.get(self.amazon_url + self.asin, headers=headers)
+        r = requests.get(self.amazon_url + self.asin, headers=self.headers)
         if r.status_code == 200:
             self.trees = html.fromstring(r.content)
         else:
@@ -49,12 +49,21 @@ class Bot(object):
                             price = self.trees.xpath(
                                 "//span[@class='a-size-base a-color-price']/text()")[0]
                         except:
-                            pass
+                            price = self.get_product_price_no_buy_box()
 
         non_decimal = re.compile(r'[^\d.]+')
         price = non_decimal.sub('', price)
 
         return round(float(price[0:5]), 2)
+
+    def get_product_price_no_buy_box(self):
+        no_bb_url = "https://www.amazon.com/gp/offer-listing/"
+        r = requests.get(no_bb_url + self.asin, headers=self.headers)
+        trees = html.fromstring(r.content)
+        price = trees.xpath(
+            "//span[@class='a-size-large a-color-price olpOfferPrice a-text-bold']/text()")
+        if price:
+            return("".join(price[0]))
 
     def get_product_name(self):
         """Returns the product name of the Amazon URL."""
@@ -71,23 +80,22 @@ class Bot(object):
     def get_number_of_sellers(self):
         no_of_sellers = 1
         """Gets Number of seller , if not found returns 1"""
-        try:
-            span = self.trees.xpath("//span[contains(text(),'New')]/text()")
-            if span:
-                extract_num = re.compile(r'[^\d.]+')
-                no_of_sellers = extract_num.sub('', span[0])
-                return int(no_of_sellers)
 
-        except:
-            span = self.trees.xpath(
-                "//span[contains(text(),'New & Used')]/text()")
-            if span:
-                extract_num = re.compile(r'[^\d.]+')
-                no_of_sellers = extract_num.sub('', span[0])
-                return int(no_of_sellers)
-            else:
-                span = "1"
-                return(span)
+        span = self.trees.xpath("//span[contains(text(),'New')]/text()")
+        if span:
+            extract_num = re.compile(r'[^\d.]+')
+            no_of_sellers = extract_num.sub('', span[0])
+            return int(no_of_sellers)
+
+        span = self.trees.xpath(
+            "//span[contains(text(),'New & Used')]/text()")
+        if span:
+            extract_num = re.compile(r'[^\d.]+')
+            no_of_sellers = extract_num.sub('', span[0])
+            return int(no_of_sellers)
+        else:
+            span = "1"
+            return(span)
 
     def search_items(self):
         if Bot.asin_check:
@@ -103,5 +111,5 @@ class Bot(object):
 
 
 if __name__ == "__main__":
-    asin = Bot('B07JXP6RW5')
+    asin = Bot('B000809OAO')
     print(asin.search_items())
